7edfc87e9d405ec17c98f78a7f0657c5c82eb973
PacoCoca <pacococacruz@gmail.com>
2020-10-09 12:58:43 +0200

    get contributions parsed

--- src/lib/getContributions.js
+++ src/lib/getContributions.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 import config from '../config';
 
-export default async function getContributions(username, from, to) {
+async function getContributionsInterval({ username, from, to }) {
   const headers = {
     'Authorization': `bearer ${config.ghToken}`,
   }
@@ -10,7 +10,6 @@ export default async function getContributions(username, from, to) {
       user(login: "${username}") {
         contributionsCollection(from: "${from}", to: "${to}") {
           contributionCalendar {
-            totalContributions
             weeks {
               contributionDays {
                 contributionCount
@@ -33,3 +32,27 @@ export default async function getContributions(username, from, to) {
   const data = await response.json();
   return data;
 };
+
+async function getContributions({ username, creationYear }) {
+  const today = new Date();
+  const contributions = [];
+
+  for (let from = creationYear; from <= today.getFullYear(); from++) {
+    // I will parse the response to [{date, count}, ...]
+    const response = await getContributionsInterval({
+      username: username,
+      from: `${from}-01-01T00:00:00`,
+      to: `${from}-12-31T23:59:59`
+    });
+
+    for (const week of response.data.user.contributionsCollection.contributionCalendar.weeks) {
+      for (const day of week.contributionDays) {
+        contributions.push({ date: day.date, count: day.contributionCount });
+      }
+    }
+  }
+
+  return contributions;
+}
+
+export default getContributions;
